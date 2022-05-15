71f01f4a0fda822751b0950a0b19508b17adbcb6
sophjane <sophiepousinho@gmail.com>
2022-05-15 22:48:39 +0100

    added lab 2 exercises

--- Lab#2/SongDetail/app/src/main/java/com/example/android/songdetailstart/SongDetailFragment.java
+++ Lab#2/SongDetail/app/src/main/java/com/example/android/songdetailstart/SongDetailFragment.java
@@ -1,24 +1,22 @@
 package com.example.android.songdetailstart;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SongDetailFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.android.songdetailstart.content.SongUtils;
+
+
 public class SongDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    public SongUtils.Song mSong;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -26,15 +24,6 @@ public class SongDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SongDetailFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SongDetailFragment newInstance(String param1, String param2) {
         SongDetailFragment fragment = new SongDetailFragment();
         Bundle args = new Bundle();
@@ -47,16 +36,29 @@ public class SongDetailFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        if (getArguments().containsKey(SongUtils.SONG_ID_KEY)) {
+            // Load the content specified by the fragment arguments.
+            mSong = SongUtils.SONG_ITEMS.get(getArguments().getInt(SongUtils.SONG_ID_KEY));
         }
     }
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song_detail, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.song_detail, container, false);
+
+        if (mSong != null) {
+            ((TextView) rootView.findViewById(R.id.song_detail)).setText(mSong.details);
+        }
+        return rootView;
+    }
+
+    public static SongDetailFragment newInstance (int selectedSong) {
+        SongDetailFragment fragment = new SongDetailFragment();
+        // Set the bundle arguments for the fragment.
+        Bundle arguments = new Bundle();
+        arguments.putInt(SongUtils.SONG_ID_KEY, selectedSong);
+        fragment.setArguments(arguments);
+        return fragment;
     }
 }
